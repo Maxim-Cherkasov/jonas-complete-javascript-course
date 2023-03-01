a3855fa33cc08d5da0c550c30ca121da6df733a8
Maxim <84859924+Maxim-Cherkasov@users.noreply.github.com>
2023-03-01 12:11:46 +0300

    DOM traversing reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,183 @@
 'use strict';
+
+//* Subjucts here is not in order, but as i learn them when i need
+
+/* 
+  We don't create closures manually like we create a new array or a new function.
+  A closure simply happens automatically in certain situations. We just need to recognize these situations.
+
+  The closure makes a function remember all the variables that existed at the function's birthplace. secureBooking is the birthplace of the func it returns, which also is booker now.
+
+  The secret of the closure is: any function always has access to the variable environment of the execution context in which the function was created.
+
+  The booker function was created in the execution context of secureBooking.
+
+  Closure: Variable Enviroment attached to the function, exactly as it was at the time and place the function was created even after that execution context is gone.
+
+  Closure gives a function access to all the variables of its parent function, even after the parent function has returned. The function keeps a reference to its outer scope, which preserves the scope chain thoughout the time.
+
+  Closure is like a backpack that has all the variables that were present in the environment where the function was created.
+
+  The booker function has access to the passengerCount variable because it's basically defined in the scope in which the booker function was created.
+
+  Now the booker function closed over its parent scope or over its parent variable evironment. And in general it incudes all functions argument though in this case we don't have any.
+
+  Closure: a function does'nt loose connection to variables that existed at the function's birthplace.
+
+  However passengerCount variable is not in the current scope and so JavaScript will immediately look into the closure and see if it can find the variable there.
+  And it does this even before looking at the scope chain.
+
+  So the closure basically has priority over the scope chain.
+*/
+
+// Closures
+
+/*
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+*/
+
+///////////////////////////////////////
+// More Closure Examples
+// Example 1
+
+// The a vaiable is in a backpack of the f function here
+
+/*
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+*/
+
+// Example 2
+
+/*
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+*/
+
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+/*
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+*/
+
+///////////////////////////////////////
+// Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+///////////////////////////////////////
+// Immediately Invoked Function Expressions (IIFE)
+
+/*
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
+
+// IIFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('This will ALSO never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+// console.log(isPrivate);
+console.log(notPrivate);
+*/
--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,14 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
+//* Modal window
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +18,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +28,274 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//! In next two sections Implementing Smooth Scrolling and Page navigation the commented code is a detailed explanations mostly of old methods so it can be useful not that often
+
+//* Implementing Smooth Scrolling
+
+// In next two sections Implementing Smooth Scrolling and Page navigation the commented code is a detailed explanations mostly of old methods so it can be useful not that often
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+// getBoundingClientRect() - is relative to a visible viewport
+// window.pageXOffset, window.pageYOffset - is relative to current scroll position. difference between top edge of a browser and the very top of a page
+
+// Button scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  // get coordinates of the element we want to scroll to
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  // Scrolling
+  // Old way:
+  // we get current position of an element and plus it to the current scroll
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // Modern way:
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//* Page navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+//* DOM Traversing.
+// Good Reference Chapter
+
+const h1 = document.querySelector('h1');
+
+// Going downwards: child
+
+// No matter how deep these child elements are, we can get them this way:
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+// Get LIVE collection of elements with .children method which is ALIVE collection which is updated. It only works for direct children.
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+
+// Direct parent:
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+// Not Direct parents:
+// We can find a parent element no matter how far away it is in the DOM tree
+// closest is opposit to querySelector. It finds parents and no matter how they up in the DOM tree
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+// the next sibling is h4
+console.log(h1.nextElementSibling);
+
+// The same methods for nodes.
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+// Here we make the all three sibling smaller 50% with scale(0.5)
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
+
+//* Dom
+
+// DOM is an interface between JS and Browser
+// API - програмный интерфейс приложения. Через него мы можем программатически взаимодействовать с DOM
+
+//* Selecting, Creating, and Deleting Elements
+// Good Reference Chapter
+
+/*
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookied for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+header.prepend(message);
+// header.append(message);
+//header.append(message.cloneNode(true));
+
+header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    // message.remove();
+    message.parentElement.removeChild(message);
+  });
+*/
+
+//* Styles, Attributes and Classes
+
+/*
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+// can't see it. it set in css class
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+// now see even css class
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+// adding 30px to a size of the meassage with px ending 44.034px - it is a string in console
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+console.log(getComputedStyle(message).height);
+
+// Changing a css variable.
+// document in js is same as root in css
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+*/
+
+// Attributes
+/*
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+// absolute path
+console.log(logo.src);
+// relative path
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+console.log(logo.className);
+
+// Don't use
+logo.clasName = 'jonas';
+*/
+
+//* Types of Events and Event Handlers
+
+/*
+// Types of Events and Event Handlers
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+
+  // if we want it occures only once
+  // h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+// if we want the addEventListener is removed after 3 seconds
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// };
+*/
+
+//* Event Propagation_ Bubbling and Capturing
+
+/*
+// Event Propagation in Practice
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
+
+// e.target is where event first happened
+// e.currentTarget is exactly in which element the event occured
+*/
