694bfdb0ad33ce3e0113761a2a5b443dd1f62933
Maxim <maxim271989@gmail.com>
2024-11-23 03:03:10 +0300

    1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,29 @@
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+const num = 1;
+const string = "string";
+const bool = true;
+
+const plus = num + string + bool;
+console.log(plus);
+console.log(typeof plus);
+console.log(bool);
